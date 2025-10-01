4585008329f92bc15423d90287bc2f23acedfbea
Hessvacio Hassan <madeinbrooklyngames@gmail.com>
2025-09-30 22:19:53 -0400

    adds week 3 lessons and labs

---
+++ Labs/lesson3-1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream> // Library to get inputs and outputs
+using namespace std;
+
+int main()
+{
+
+  int coffeeBronx = 1;
+  int coffeeBrooklyn = 2;
+
+  /*...
+  Write code that will make the variable coffeeBronx the value of coffeeBrooklyn 
+  & the variable of coffeeBrooklyn the value of coffeeBronx.
+
+
+
+
+
+
+
+
+
+
+
+
+  ....*/
+  std::cout << "coffeeBronx is " << coffeeBronx << " & coffeeBrooklyn is " << coffeeBrooklyn << endl;
+  return 0;
+}
+
